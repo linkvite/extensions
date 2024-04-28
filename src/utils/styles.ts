64ffbde894cd10d2505061833183daab8ce4cf08
Kayode Ogunmakinwa <khizzyjr@gmail.com>
2024-04-27 23:39:38 -0400

    chore: removed unused styles

--- src/utils/styles.ts
+++ src/utils/styles.ts
@@ -1,48 +1,31 @@
 export const Colors = {
-    primary: "#FA5A5F",
-    trans: "rgba(0, 0, 0, 0.4)",
-
-    light: "#FFFFFF",
-    light_sub: "#F9F9F9",
-    light_text: "#202225",
-
     dark: "#212121",
-    dark_sub: "#757575",
-    dark_text: "#FFFFFF",
-    dark_text_sub: "#bdbdbd",
-
-    safe: "#2196f3",
-    safe_mid: "#1565c0",
+    light: "#FFFFFF",
     error: "#ff4747",
-    warning: "#ffc007",
     orange: "#ff9800",
+    warning: "#ffc007",
+    primary: "#FA5A5F",
     success: "#1DB954",
-    border_color: "#bdbdbd",
-    purple: "#5e35b1",
-    indigo: "#3f51b5",
-};
+    dark_sub: "#757575",
+} as const;
 
 export const LightTheme = {
-    text: Colors.light_text,
+    text: "#202225",
     text_sub: Colors.dark_sub,
-
     background: Colors.light,
-    background_sub: Colors.light_sub,
-
+    background_sub: "#F9F9F9",
     trans_bg: "rgba( 229, 229, 234, 0.5 )",
     trans_bg_opp: "rgba(0,0,0,0.1)"
-};
+} as const;
 
 export const DarkTheme = {
-    text: Colors.dark_text,
-    text_sub: Colors.dark_text_sub,
-
+    text: "#FFFFFF",
+    text_sub: "#bdbdbd",
     background: "#000000",
     background_sub: "#1A1A1A",
-
     trans_bg: "rgba( 72, 72, 74, 0.4 )",
     trans_bg_opp: "rgba( 209, 209, 214, 0.15 )"
-};
+} as const;
 
 export type ITheme = typeof LightTheme | typeof DarkTheme;
 
